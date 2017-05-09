e5a26e39d54fd497b2aa27b42e898d1a40361e09
hq <1109806140@qq.com>
2017-05-09 12:20:53 +0800

    添加nav组件

--- src/js/directive/navDir.js
+++ src/js/directive/navDir.js
@@ -1,3 +1,12 @@
 /**
  * Created by hq on 2017/5/9.
  */
+(function (angular) {
+    'use strict';
+    angular.module('app').directive('nav',function () {
+        return {
+            restrict:'EA',
+            templateUrl:'../view/tpl/nav-tpl.html'
+        }
+    })
+})(angular);
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by hq on 2017/5/9.
  */
+(function (angular) {
+    'use strict';
+    var app = angular.module('app',[]);
+    app.controller('appController',['$scope',function ($scope) {
+        $scope.name = '今日一刻';
+    }])
+})(angular);
